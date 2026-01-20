e115da4a0ffebebfef089f9dd7e0273993a4c6a9
Markus Konojacki <markus@konojacki.de>
2026-01-21 00:29:32 +0100

    .

--- bash_aliases.nix
+++ bash_aliases.nix
@@ -9,7 +9,7 @@
     fu="sudo $(fc -ln -1)";
     sudo="sudo ";
     updt="cd /etc/nixos && sudo git pull && sudo nixos-rebuild switch --upgrade";
-    test="cd /etc/nixos && sudo git pull && sudo nixos-rebuild test";
+    tst="cd /etc/nixos && sudo git pull && sudo nixos-rebuild test";
     untar="tar -xzf";
     tarit="tar -cvzf";
     cp="cp -riv";
